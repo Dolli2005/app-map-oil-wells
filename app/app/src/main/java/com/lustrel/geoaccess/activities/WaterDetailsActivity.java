14c10ccd6a3fd23ef9ee22c10c0161a37055c39c
Stanley Sathler <stanleysathlerpinto@gmail.com>
2016-11-29 16:54:36 -0200

    Finalizado tela de pocos de agua, faltando imagem

--- app/app/src/main/java/com/lustrel/geoaccess/activities/WaterDetailsActivity.java
+++ app/app/src/main/java/com/lustrel/geoaccess/activities/WaterDetailsActivity.java
@@ -1,17 +1,21 @@
 package com.lustrel.geoaccess.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telecom.Call;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lustrel.geoaccess.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class WaterDetailsActivity extends Activity {
+public class WaterDetailsActivity extends AppCompatActivity {
 
-    private TextView lblName;
+    private Toolbar toolbar;
     private TextView lblLatitude;
     private TextView lblLongitude;
     private TextView lblState;
@@ -28,14 +32,13 @@ public class WaterDetailsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.water_details_activity);
-
-        getAllViewElements();
+        loadElementsFromXML();
+        applyToolbar();
         populateDetails();
     }
 
-    private void getAllViewElements(){
-        /*
-        lblName = (TextView) findViewById(R.id.name);
+    private void loadElementsFromXML(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         lblLatitude = (TextView) findViewById(R.id.latitude);
         lblLongitude = (TextView) findViewById(R.id.longitude);
         lblState = (TextView) findViewById(R.id.state);
@@ -45,30 +48,45 @@ public class WaterDetailsActivity extends Activity {
         lblStaticLevel = (TextView) findViewById(R.id.static_level);
         lblDynamicLevel = (TextView) findViewById(R.id.dynamic_level);
         lblFlowNumber = (TextView) findViewById(R.id.flow_number);
-        lblPresentSolids = (TextView) findViewById(R.id.present_solids);
+        lblPresentSolids = (TextView) findViewById(R.id.solids_present);
         lblFlowRate = (TextView) findViewById(R.id.flow_rate);
-        */
+    }
+
+    private void applyToolbar(){
+        setSupportActionBar(toolbar);
     }
 
     private void populateDetails(){
-        /*
         try {
             String detailsAsText = getIntent().getStringExtra("MARKER_DETAILS");
             JSONObject details = new JSONObject(detailsAsText);
 
-            lblName.setText(details.getString("name"));
-            lblLatitude.setText(getString(R.string.detail_latitude_text) + " " + details.getString("latitude"));
-            lblLongitude.setText(getString(R.string.detail_longitude_text) + " " + details.getString("longitude"));
-            lblState.setText(getString(R.string.detail_state_text) + " " + details.getString("state"));
-            lblLocation.setText(getString(R.string.detail_location_text) + " " + details.getString("location"));
-            lblCounty.setText(getString(R.string.detail_county_text) + " " + details.getString("county"));
-            lblDepth.setText(getString(R.string.detail_depth_text) + " " + details.getString("depth"));
-            lblStaticLevel.setText(getString(R.string.detail_static_level_text) + " " + details.getString("nivelestatico"));
-            lblDynamicLevel.setText(getString(R.string.detail_dynamic_level_text) + " " + details.getString("niveldinamico"));
-            lblFlowNumber.setText(getString(R.string.detail_flow_number_text) + " " + details.getString("numerodavazao"));
-            lblPresentSolids.setText(getString(R.string.detail_present_solids_text) + " " + details.getString("solidspresent"));
-            lblFlowRate.setText(getString(R.string.detail_flow_rate_text) + " " + details.getString("flowrate"));
-        } catch(JSONException exception){}
-        */
+            setTitle(details.getString("name"));
+            if(details.has("latitude"))
+                lblLatitude.setText(getString(R.string.detail_latitude_text) + " " + details.getString("latitude"));
+            if(details.has("longitude"))
+                lblLongitude.setText(getString(R.string.detail_longitude_text) + " " + details.getString("longitude"));
+            if(details.has("state"))
+                lblState.setText(getString(R.string.detail_state_text) + " " + details.getString("state"));
+            if(details.has("location"))
+                lblLocation.setText(getString(R.string.detail_location_text) + " " + details.getString("location"));
+            if(details.has("county"))
+                lblCounty.setText(getString(R.string.detail_county_text) + " " + details.getString("county"));
+            if(details.has("depth"))
+                lblDepth.setText(getString(R.string.detail_depth_text) + " " + details.getString("depth"));
+            if(details.has("nivelestatico"))
+                lblStaticLevel.setText(getString(R.string.detail_static_level_text) + " " + details.getString("nivelestatico"));
+            if(details.has("niveldinamico"))
+                lblDynamicLevel.setText(getString(R.string.detail_dynamic_level_text) + " " + details.getString("niveldinamico"));
+            if(details.has("numerodavazao"))
+                lblFlowNumber.setText(getString(R.string.detail_flow_number_text) + " " + details.getString("numerodavazao"));
+            if(details.has("solidspresent"))
+                lblPresentSolids.setText(getString(R.string.detail_present_solids_text) + " " + details.getString("solidspresent"));
+            if(details.has("flowrate"))
+                lblFlowRate.setText(getString(R.string.detail_flow_rate_text) + " " + details.getString("flowrate"));
+        } catch(JSONException exception){
+            Log.e("H", "H");
+            Toast.makeText(this, "What", Toast.LENGTH_SHORT).show();
+        }
     }
 }
